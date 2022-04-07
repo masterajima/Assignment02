010f325e91a65ad100babfe3a97c92366250530b
masterajima <98704309+masterajima@users.noreply.github.com>
2022-04-07 10:56:13 -0400

    edit item in fooditem

---
+++ FoodItem.cs
@@ -0,0 +1,9 @@
+using System;
+namespace ajimaGroceryStoreApp.FoodItem{
+    public class FoodItem{
+        public int ID{get;set;}
+        public string Name{get;set;}
+        public string Description{get;set;}
+        public decimal price{get;set;}
+    }
+}
